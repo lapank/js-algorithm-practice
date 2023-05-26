efc72f0ec3478c00de66a2da93a6087087a6eb49
Kaylin Lapan <kaylinlapan@gmail.com>
2023-05-26 17:44:18 -0400

    Stack add and delete function added to linked list.

--- scripts.js
+++ scripts.js
@@ -14,7 +14,36 @@ function linkedList(){
         constructor(head){
             this.head = head;
         }
-    }
+        getFirst(){
+            return this.head;
+        }
+        getLast(){
+            let lastNode = this.head;
+            while(lastNode.next){
+                lastNode = lastNode.next;
+            }
+            return lastNode;
+            }
+        stackAdd(x){
+            let oldHead = this.head;
+            console.log("oldHead " + oldHead.data);
+            let newNode = new Node(x);
+            console.log("newNode " + newNode.data);
+            newNode.next = oldHead;
+            console.log("newNode next " + newNode.next.data);
+            this.head = newNode;
+            console.log("listHead " + this.head.data);
+        }
+        stackDelete(){
+            let oldHead = this.head;
+            let newHead = this.head.next;
+            delete this.head;
+            this.head = newHead;
+        }
+        queueAdd(){}
+        queueDelete(){}
+
+        }
 
     class Node{
         constructor(data){
@@ -23,13 +52,21 @@ function linkedList(){
         }
     }
 
+    
+
+
     let node1 = new Node(3);
     let node2 = new Node(7);
     node1.next = node2;
 
     let list1 = new LinkedList(node1);
+    list1.stackAdd(12);
 
-    console.log(list1.head.next.data);
+    console.log("First Node " + list1.getFirst().data);
+    console.log("Last Node " + list1.getLast().data);
+    list1.stackDelete();
+    console.log("New First Node " + list1.getFirst().data);
+
 }
 linkedList();
 
@@ -78,7 +115,7 @@ function queue(){
     queue1.enqueue("dogs");
 
     console.log(queue1.printQueue());
-    
+
     queue1.enqueue("cats");
     queue1.enqueue("frogs");
     queue1.enqueue("ponies");
@@ -96,6 +133,31 @@ queue();
 
 // Stack
 
+function stack(){
+    class Stack{
+        constructor(){
+            this.data = [];
+        }
+        push(){
+            this.data
+        }
+        pop(){
+
+        }
+        reverse(){
+
+        }
+        isEmpty(){
+
+        }
+        print(){
+
+        }
+        size(){
+
+        }
+    }
+}
 // Hash Table
 
 // Graph
